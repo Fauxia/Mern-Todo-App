cc1dce28872b09334e190c098c37dbb840c4c690
Fauxia <135736639+Fauxia@users.noreply.github.com>
2024-08-29 11:05:49 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,6 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use("/api", productRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname + "/frontend/dist/index.html"));
-  });
-}
 
 const PORT = process.env.PORT || 4000;
 
