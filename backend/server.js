4b523d2954e3209f81107d553b58bc2908efa9ff
Husnain Ali <fauxia39@gmail.com>
2024-08-30 02:08:41 +0500

    added

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ import dotenv from "dotenv";
 import { connectToDb } from "./config/db.js";
 import { productRouter } from "./routes/product.route.js";
 import path from "path";
+import cors from "cors";
 
 dotenv.config();
+
+app.use(cors());
 
 const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use("/api", productRouter);
-
 
 const PORT = process.env.PORT || 4000;
 
